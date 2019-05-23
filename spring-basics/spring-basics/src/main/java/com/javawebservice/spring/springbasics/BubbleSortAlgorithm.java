132a6a79dd273623de08664e636ea7f28a180865
Yuvan <yuvanpradeep594@gmail.com>
2019-05-22 17:10:40 -0700

    Clean the master branch

--- spring-basics/spring-basics/src/main/java/com/javawebservice/spring/springbasics/BubbleSortAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package com.javawebservice.spring.springbasics;
-
-public class BubbleSortAlgorithm implements SortAlgorithm{
-
-	public int [] sortAl(int [] inputNumbers) {
-		return inputNumbers;
-	}
-}
--- spring-basics/spring-basics/src/main/java/com/javawebservice/spring/springbasics/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.javawebservice.spring.springbasics;
-
-public interface SortAlgorithm {
-	public int[] sortAl(int[] numbers);
-}
