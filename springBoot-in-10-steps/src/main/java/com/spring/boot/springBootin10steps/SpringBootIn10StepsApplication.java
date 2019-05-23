a7e87cdd607e2ad41a1fa515adcf0302ee3fccb5
Yuvan <yuvanpradeep594@gmail.com>
2019-05-23 11:52:22 -0700

    spring boot initial commit

---
+++ springBoot-in-10-steps/src/main/java/com/spring/boot/springBootin10steps/SpringBootIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springBootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn10StepsApplication.class, args);
+	}
+
+}
