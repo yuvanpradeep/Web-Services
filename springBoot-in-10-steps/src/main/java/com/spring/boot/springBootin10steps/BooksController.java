630050606adaf10623cdedef27976290d8fd6987
Yuvan <yuvanpradeep594@gmail.com>
2019-05-23 12:29:04 -0700

    Created sample rest controller to return list of book details (endpoint = "/books")

---
+++ springBoot-in-10-steps/src/main/java/com/spring/boot/springBootin10steps/BooksController.java
@@ -0,0 +1,16 @@
+package com.spring.boot.springBootin10steps;
+
+import java.util.List;
+import java.util.Arrays;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks() {
+		return Arrays.asList(new Book(1l, "CSE 240", "Chen"));
+	}
+}
