2ab3b7bd5e9602ae61297f3391b441c049870fdf
Yuvan <yuvanpradeep594@gmail.com>
2019-05-24 16:22:48 -0700

    Usage of path variable

--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/HellowordController.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/HellowordController.java
@@ -1,6 +1,7 @@
 package com.java.restful.RESTfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class HellowordController {
 	@RequestMapping(method = RequestMethod.GET, path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello ----- World");
+	}
+	
+	// With path parameter
+	@RequestMapping(method = RequestMethod.GET, path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanParam(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
  }
