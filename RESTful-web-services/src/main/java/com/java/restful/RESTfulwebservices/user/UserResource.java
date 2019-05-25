16da8814cec40a08216b60b961343f761ae2f0dd
Yuvan <yuvanpradeep594@gmail.com>
2019-05-24 17:27:55 -0700

    Code for retreiving particular user.

--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/user/UserResource.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/user/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class UserResource {
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
+	}
+	
+	@GetMapping("/user/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
 	}
 }
