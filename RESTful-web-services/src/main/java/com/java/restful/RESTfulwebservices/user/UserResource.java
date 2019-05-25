cb45950d1a16d1dadee3a81188fe55ba0f462fd1
Yuvan <yuvanpradeep594@gmail.com>
2019-05-24 17:16:59 -0700

    Added DAO layer to get the static data.

---
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/user/UserResource.java
@@ -0,0 +1,20 @@
+package com.java.restful.RESTfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService service;
+	 
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+}
