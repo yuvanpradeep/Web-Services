0dbde69044aec85f5c76970c4639989b4976d4ab
Yuvan <yuvanpradeep594@gmail.com>
2019-05-24 19:41:10 -0700

    Implementing Exception Handling

--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/user/UserResource.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.java.restful.RESTfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -23,11 +26,24 @@ public class UserResource {
 	
 	@GetMapping("/user/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
+		// return status after created
+		// return URI
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+			
 	}
 }
