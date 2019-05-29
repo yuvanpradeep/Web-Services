ad50ce1cb286a95ef91da4d2c98076a58bd7911e
Yuvan <yuvanpradeep594@gmail.com>
2019-05-28 21:15:06 -0700

    Internalization has been added using locale concept

--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/ResTfulWebServicesApplication.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/ResTfulWebServicesApplication.java
@@ -1,7 +1,12 @@
 package com.java.restful.RESTfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class ResTfulWebServicesApplication {
@@ -10,4 +15,17 @@ public class ResTfulWebServicesApplication {
 		SpringApplication.run(ResTfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource bundlemessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/helloworld/HellowordController.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/helloworld/HellowordController.java
@@ -1,7 +1,12 @@
 package com.java.restful.RESTfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HellowordController {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	// GET
 	// URI helloworld
 	//@GetMapping("/hello-world")
 	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
 	public String helloWorld() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/hello-world-internalization")
+	public String greetingInternational(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message",null, locale);
 	}
 	
 	// Get bean 
