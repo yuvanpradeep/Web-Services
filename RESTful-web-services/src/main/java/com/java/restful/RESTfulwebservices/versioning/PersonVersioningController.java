8477bae9c8b461f47134b1224bf1ed13a0c4764b
Yuvan <yuvanpradeep594@gmail.com>
2019-05-29 21:02:23 -0700

    Different versioning technique based on usecase.

--- RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/versioning/PersonVersioningController.java
+++ RESTful-web-services/src/main/java/com/java/restful/RESTfulwebservices/versioning/PersonVersioningController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 	
+// Versioning using URI	
 @GetMapping("v1/person")
 public PersonV1 personV1() {
 	return new PersonV1("Yuvan Pradeep");
@@ -15,4 +16,38 @@ public PersonV1 personV1() {
 public PersonV2 personV2() {
 	return new PersonV2(new Name("Yuvan","Pradeep"));
 }
+
+// Versioning using params
+@GetMapping(value = "/person/param", params = "version=1")
+public PersonV1 paramV1() {
+	return new PersonV1("Yuvan Pradeep");
+}
+
+@GetMapping(value = "/person/param", params = "version=2")
+public PersonV2 paramV2() {
+	return new PersonV2(new Name("Yuvan","Pradeep"));
+}
+
+// Versioning using headers
+@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+public PersonV1 headerV1() {
+	return new PersonV1("Yuvan Pradeep");
+}
+
+@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+public PersonV2 headerV2() {
+	return new PersonV2(new Name("Yuvan","Pradeep"));
+}
+
+// Versioning using produces also called accept header versioning
+@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+public PersonV1 producesV1() {
+	return new PersonV1("Yuvan Pradeep");
+}
+
+@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+public PersonV2 producesV2() {
+	return new PersonV2(new Name("Yuvan","Pradeep"));
+}
+
 }
