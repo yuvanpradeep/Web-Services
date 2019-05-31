a181b5884c8b95aacd2efb328611fadc3b696803
Yuvan <yuvanpradeep594@gmail.com>
2019-05-31 16:32:57 -0700

    Command line runner to save the data

---
+++ JPA-project/src/main/java/com/web/jpa/JPAproject/UserDAOServiceCommandLineRunner.java
@@ -0,0 +1,29 @@
+package com.web.jpa.JPAproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.web.jpa.JPAproject.entity.User;
+import com.web.jpa.JPAproject.service.UserDAOService;
+
+
+@Component
+public class UserDAOServiceCommandLineRunner implements CommandLineRunner{
+
+	private static final Logger log = LoggerFactory.getLogger(UserDAOServiceCommandLineRunner.class);
+	
+	
+	@Autowired
+	private UserDAOService userDaoService;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("Yuvan","Admin");
+		long userID = userDaoService.insert(user);
+		log.info("New user is created" + user);
+	}
+}
+ 
