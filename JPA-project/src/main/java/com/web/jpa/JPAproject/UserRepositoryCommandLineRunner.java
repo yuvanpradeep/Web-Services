8b7b021cddd8c116ff52a44a88a9b16a722748e2
Yuvan <yuvanpradeep594@gmail.com>
2019-05-31 17:20:54 -0700

    Implemented few functions like findbyid and findall.

--- JPA-project/src/main/java/com/web/jpa/JPAproject/UserRepositoryCommandLineRunner.java
+++ JPA-project/src/main/java/com/web/jpa/JPAproject/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.web.jpa.JPAproject;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +15,9 @@ import com.web.jpa.JPAproject.service.UserRepository;
 
 
 @Component
-public class UserDAOServiceCommandLineRunner implements CommandLineRunner{
+public class UserRepositoryCommandLineRunner implements CommandLineRunner{
 
-	private static final Logger log = LoggerFactory.getLogger(UserDAOServiceCommandLineRunner.class);
+	private static final Logger log = LoggerFactory.getLogger(UserRepositoryCommandLineRunner.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -23,7 +26,10 @@ public class UserDAOServiceCommandLineRunner implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		User user = new User("Yuvan","Admin");
 		userRepository.save(user);
-		log.info("New user is created" + user);
+		Optional<User> userWithIdOne = userRepository.findById(1L);
+		log.info("New user is retrived" + user);
+		List<User> users = userRepository.findAll();
+		log.info("ALl users" + users);
 	}
 }
  
